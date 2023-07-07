77461fb25f6b6fffd313eef749760fcbcf24128a
Junaith1994 <daya2018ctg@gmail.com>
2023-07-07 16:25:37 +0600

    Added navigation bar with styles and added not found page

---
+++ src/components/404Page/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className=''>
+            <h1 className='fw-bold'>Page Not Found !! <span className='text-danger'>404 !!!</span></h1>
+        </div>
+    );
+};
+
+export default NotFound;
