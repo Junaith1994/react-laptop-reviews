39363b208e86d23e05b2694ad4391962988f9513
Junaith1994 <daya2018ctg@gmail.com>
2023-07-11 00:09:30 +0600

    Added info in About page, Added project info in README.MD file

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is About</h1>
+        <div className='container my-3'>
+            <h1 className='about-heading'>About Us</h1>
+            <p className=''>
+                <span className='text-primary fw-bold'>LAPTOP REVIEW</span> page was created on july 2023. The purpose is to help thousands of students and professionals who are working individually in their own working fields with laptop or pc, a friend who helps us daily in many tasks.
+                So We are here to provide them authentic information by thousands of varified users about many laptops and make their decision making easier.
+            </p>
         </div>
     );
 };
